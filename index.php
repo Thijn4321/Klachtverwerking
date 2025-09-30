1a22022cefc47c22ca21f5e0a101666217159f8c
thijn libregts <thijnlibregts@gmail.com>
2025-09-30 10:18:22 +0200

    Initial commit with project code

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+require '../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$CC = "thijnlibregts@gmail.com";
+$onderwerp = "Klacht";
+echo "<form action='./index.php' method= 'POST'>
+        <label for=''>Naam</label>
+        <input type='text' name='Naam' required>
+        <br>
+        <label for='Email'>Email</label>
+        <input type='email' name='Email' required>
+        <br>
+        <label for='Beschrijvingklacht'>Omschrijf je klacht</label>
+        <input type='text' name='BeschrijvingKlacht' required>
+        <br>
+        <input type='submit' value='verstuur'>
+    </form>";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $naam_user = $_POST["Naam"];
+    $email_user = $_POST["Email"];
+    $beschrijvingklacht = $_POST["BeschrijvingKlacht"];
+    $mail = new PHPMailer(true);
+    try {
+        // SMTP instellingen voor Gmail
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'thijnlibregts@gmail.com'; // jouw Gmailadres
+        $mail->Password   = 'dovj advr nzpl bmdw';      // Gmail app-wachtwoord
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+        // Ontvangers
+        $mail->setFrom('no-reply@gmail.com', 'Uw Bedrijf'); // afzender
+        $mail->addAddress($email_user, $naam_user);          // gebruiker
+        $mail->addCC($CC);                                   // jezelf in cc
+
+        // Inhoud
+        $mail->isHTML(false); // platte tekst
+        $mail->Subject = 'Uw klacht is in behandeling';
+        $mail->Body    = "Beste $naam_user,\n\nBedankt voor uw inzending.\n\nNaam: $naam_user\nE-mail: $email_user\nBericht: $beschrijvingklacht\n\nMet vriendelijke groet,\nUw Bedrijf";
+
+        $mail->send();
+        echo "Bericht is verzonden!";
+    } catch (Exception $e) {
+        echo "Bericht kon niet worden verzonden. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+
+?>
